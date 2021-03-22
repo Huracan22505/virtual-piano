c78dddaef51c42fe08ceb7c5c1ac28f9847140b7
Alexander Huracan <Nokia2611@mail.ru>
2021-03-22 22:02:28 +0200

    feat: add keyboard logic

--- virtual-piano/js/index.js
+++ virtual-piano/js/index.js
@@ -63,107 +63,107 @@ function onMouseOver(e) {
   }
 }
 
-// window.addEventListener("keydown", (event) => {
-//   if (event.repeat) return;
+window.addEventListener("keydown", (event) => {
+  if (event.repeat) return;
 
-//   if (event.code === "KeyD") {
-//     const src = `../assets/audio/c.mp3`;
-//     playAudio(src);
-//     [...pianoКeys]
-//       .find((el) => el.dataset.letter === "D")
-//       .classList.add("piano-key-active");
-//   }
+  if (event.code === "KeyD") {
+    const src = `../assets/audio/c.mp3`;
+    playAudio(src);
+    [...pianoКeys]
+      .find((el) => el.dataset.letter === "D")
+      .classList.add("piano-key-active");
+  }
 
-//   if (event.code === "KeyF") {
-//     const src = `../assets/audio/d.mp3`;
-//     playAudio(src);
-//     [...pianoКeys]
-//       .find((el) => el.dataset.letter === "F")
-//       .classList.add("piano-key-active");
-//   }
+  if (event.code === "KeyF") {
+    const src = `../assets/audio/d.mp3`;
+    playAudio(src);
+    [...pianoКeys]
+      .find((el) => el.dataset.letter === "F")
+      .classList.add("piano-key-active");
+  }
 
-//   if (event.code === "KeyG") {
-//     const src = `../assets/audio/e.mp3`;
-//     playAudio(src);
-//     [...pianoКeys]
-//       .find((el) => el.dataset.letter === "G")
-//       .classList.add("piano-key-active");
-//   }
+  if (event.code === "KeyG") {
+    const src = `../assets/audio/e.mp3`;
+    playAudio(src);
+    [...pianoКeys]
+      .find((el) => el.dataset.letter === "G")
+      .classList.add("piano-key-active");
+  }
 
-//   if (event.code === "KeyH") {
-//     const src = `../assets/audio/f.mp3`;
-//     playAudio(src);
-//     [...pianoКeys]
-//       .find((el) => el.dataset.letter === "H")
-//       .classList.add("piano-key-active");
-//   }
+  if (event.code === "KeyH") {
+    const src = `../assets/audio/f.mp3`;
+    playAudio(src);
+    [...pianoКeys]
+      .find((el) => el.dataset.letter === "H")
+      .classList.add("piano-key-active");
+  }
 
-//   if (event.code === "KeyJ") {
-//     const src = `../assets/audio/g.mp3`;
-//     playAudio(src);
-//     [...pianoКeys]
-//       .find((el) => el.dataset.letter === "J")
-//       .classList.add("piano-key-active");
-//   }
+  if (event.code === "KeyJ") {
+    const src = `../assets/audio/g.mp3`;
+    playAudio(src);
+    [...pianoКeys]
+      .find((el) => el.dataset.letter === "J")
+      .classList.add("piano-key-active");
+  }
 
-//   if (event.code === "KeyK") {
-//     const src = `../assets/audio/a.mp3`;
-//     playAudio(src);
-//     [...pianoКeys]
-//       .find((el) => el.dataset.letter === "K")
-//       .classList.add("piano-key-active");
-//   }
+  if (event.code === "KeyK") {
+    const src = `../assets/audio/a.mp3`;
+    playAudio(src);
+    [...pianoКeys]
+      .find((el) => el.dataset.letter === "K")
+      .classList.add("piano-key-active");
+  }
 
-//   if (event.code === "KeyL") {
-//     const src = `../assets/audio/b.mp3`;
-//     playAudio(src);
-//     [...pianoКeys]
-//       .find((el) => el.dataset.letter === "L")
-//       .classList.add("piano-key-active");
-//   }
+  if (event.code === "KeyL") {
+    const src = `../assets/audio/b.mp3`;
+    playAudio(src);
+    [...pianoКeys]
+      .find((el) => el.dataset.letter === "L")
+      .classList.add("piano-key-active");
+  }
 
-//   // sharps
+  // sharps
 
-//   if (event.code === "KeyR") {
-//     const src = `../assets/audio/c♯.mp3`;
-//     playAudio(src);
-//     [...pianoКeys]
-//       .find((el) => el.dataset.letter === "R")
-//       .classList.add(`piano-key-active`);
-//   }
-//   if (event.code === "KeyT") {
-//     const src = `../assets/audio/d♯.mp3`;
-//     playAudio(src);
-//     [...pianoКeys]
-//       .find((el) => el.dataset.letter === "T")
-//       .classList.add("piano-key-active");
-//   }
-//   if (event.code === "KeyU") {
-//     const src = `../assets/audio/f♯.mp3`;
-//     playAudio(src);
-//     [...pianoКeys]
-//       .find((el) => el.dataset.letter === "U")
-//       .classList.add("piano-key-active");
-//   }
-//   if (event.code === "KeyI") {
-//     const src = `../assets/audio/g♯.mp3`;
-//     playAudio(src);
-//     [...pianoКeys]
-//       .find((el) => el.dataset.letter === "I")
-//       .classList.add("piano-key-active");
-//   }
-//   if (event.code === "KeyO") {
-//     const src = `../assets/audio/a♯.mp3`;
-//     playAudio(src);
-//     [...pianoКeys]
-//       .find((el) => el.dataset.letter === "O")
-//       .classList.add("piano-key-active");
-//   }
+  if (event.code === "KeyR") {
+    const src = `../assets/audio/c♯.mp3`;
+    playAudio(src);
+    [...pianoКeys]
+      .find((el) => el.dataset.letter === "R")
+      .classList.add(`piano-key-active`);
+  }
+  if (event.code === "KeyT") {
+    const src = `../assets/audio/d♯.mp3`;
+    playAudio(src);
+    [...pianoКeys]
+      .find((el) => el.dataset.letter === "T")
+      .classList.add("piano-key-active");
+  }
+  if (event.code === "KeyU") {
+    const src = `../assets/audio/f♯.mp3`;
+    playAudio(src);
+    [...pianoКeys]
+      .find((el) => el.dataset.letter === "U")
+      .classList.add("piano-key-active");
+  }
+  if (event.code === "KeyI") {
+    const src = `../assets/audio/g♯.mp3`;
+    playAudio(src);
+    [...pianoКeys]
+      .find((el) => el.dataset.letter === "I")
+      .classList.add("piano-key-active");
+  }
+  if (event.code === "KeyO") {
+    const src = `../assets/audio/a♯.mp3`;
+    playAudio(src);
+    [...pianoКeys]
+      .find((el) => el.dataset.letter === "O")
+      .classList.add("piano-key-active");
+  }
 
-//   window.addEventListener("keyup", () => {
-//     [...pianoКeys].map((el) => el.classList.remove("piano-key-active"));
-//   });
-// });
+  window.addEventListener("keyup", () => {
+    [...pianoКeys].map((el) => el.classList.remove("piano-key-active"));
+  });
+});
 
 function playAudio(src) {
   const audio = new Audio();
