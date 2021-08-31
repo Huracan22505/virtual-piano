72a70f5aecefda5d9ed81e7c4caf7742cd069861
Huracan22505 <nokia2611@mail.ru>
2021-08-31 12:23:17 +0300

    refactor: structure

--- virtual-piano/js/index.js
+++ virtual-piano/js/index.js
@@ -1,5 +1,5 @@
 const piano = document.querySelector(".piano");
-const pianoКeys = document.querySelectorAll(".piano-key");
+const pianoKeys = document.querySelectorAll(".piano-key");
 const lettersBtn = document.querySelector(".btn-letters");
 const notesBtn = document.querySelector(".btn-notes");
 const fullscreenBtn = document.querySelector(".fullscreen");
@@ -12,13 +12,13 @@ fullscreenBtn.addEventListener("click", () => {
 });
 
 lettersBtn.addEventListener("click", (e) => {
-  [...pianoКeys].map((el) => el.classList.add("piano-key-letter"));
+  [...pianoKeys].map((el) => el.classList.add("piano-key-letter"));
   e.target.classList.add("btn-active");
   notesBtn.classList.remove("btn-active");
 });
 
 notesBtn.addEventListener("click", (e) => {
-  [...pianoКeys].map((el) => el.classList.remove("piano-key-letter"));
+  [...pianoKeys].map((el) => el.classList.remove("piano-key-letter"));
   e.target.classList.add("btn-active");
   lettersBtn.classList.remove("btn-active");
 });
@@ -39,7 +39,7 @@ piano.addEventListener("mousedown", (e) => {
   window.addEventListener("mouseup", () => {
     piano.removeEventListener("mouseover", onMouseOver);
 
-    pianoКeys.forEach((el) => {
+    pianoKeys.forEach((el) => {
       if (el.classList.contains("piano-key-active")) {
         el.classList.remove("piano-key-active");
       }
@@ -49,7 +49,7 @@ piano.addEventListener("mousedown", (e) => {
 
 function onMouseOver(e) {
   if (e.target.classList.contains("piano-key")) {
-    pianoКeys.forEach((el) => {
+    pianoKeys.forEach((el) => {
       if (el.classList.contains("piano-key-active")) {
         el.classList.remove("piano-key-active");
       }
@@ -66,103 +66,111 @@ function onMouseOver(e) {
 window.addEventListener("keydown", (event) => {
   if (event.repeat) return;
 
-  if (event.code === "KeyD") {
+  const { code } = event;
+  console.log(
+    "🚀 ~ file: index.js ~ line 70 ~ window.addEventListener ~ code",
+    code
+  );
+  const note = event.currentTarget;
+  console.log(note);
+
+  if (code === "KeyD") {
     const src = `./assets/audio/c.mp3`;
     playAudio(src);
-    [...pianoКeys]
+    [...pianoKeys]
       .find((el) => el.dataset.letter === "D")
       .classList.add("piano-key-active");
   }
 
-  if (event.code === "KeyF") {
+  if (code === "KeyF") {
     const src = `./assets/audio/d.mp3`;
     playAudio(src);
-    [...pianoКeys]
+    [...pianoKeys]
       .find((el) => el.dataset.letter === "F")
       .classList.add("piano-key-active");
   }
 
-  if (event.code === "KeyG") {
+  if (code === "KeyG") {
     const src = `./assets/audio/e.mp3`;
     playAudio(src);
-    [...pianoКeys]
+    [...pianoKeys]
       .find((el) => el.dataset.letter === "G")
       .classList.add("piano-key-active");
   }
 
-  if (event.code === "KeyH") {
+  if (code === "KeyH") {
     const src = `./assets/audio/f.mp3`;
     playAudio(src);
-    [...pianoКeys]
+    [...pianoKeys]
       .find((el) => el.dataset.letter === "H")
       .classList.add("piano-key-active");
   }
 
-  if (event.code === "KeyJ") {
+  if (code === "KeyJ") {
     const src = `./assets/audio/g.mp3`;
     playAudio(src);
-    [...pianoКeys]
+    [...pianoKeys]
       .find((el) => el.dataset.letter === "J")
       .classList.add("piano-key-active");
   }
 
-  if (event.code === "KeyK") {
+  if (code === "KeyK") {
     const src = `./assets/audio/a.mp3`;
     playAudio(src);
-    [...pianoКeys]
+    [...pianoKeys]
       .find((el) => el.dataset.letter === "K")
       .classList.add("piano-key-active");
   }
 
-  if (event.code === "KeyL") {
+  if (code === "KeyL") {
     const src = `./assets/audio/b.mp3`;
     playAudio(src);
-    [...pianoКeys]
+    [...pianoKeys]
       .find((el) => el.dataset.letter === "L")
       .classList.add("piano-key-active");
   }
 
   // sharps
 
-  if (event.code === "KeyR") {
+  if (code === "KeyR") {
     const src = `./assets/audio/c♯.mp3`;
     playAudio(src);
-    [...pianoКeys]
+    [...pianoKeys]
       .find((el) => el.dataset.letter === "R")
       .classList.add(`piano-key-active`);
   }
-  if (event.code === "KeyT") {
+  if (code === "KeyT") {
     const src = `./assets/audio/d♯.mp3`;
     playAudio(src);
-    [...pianoКeys]
+    [...pianoKeys]
       .find((el) => el.dataset.letter === "T")
       .classList.add("piano-key-active");
   }
-  if (event.code === "KeyU") {
+  if (code === "KeyU") {
     const src = `./assets/audio/f♯.mp3`;
     playAudio(src);
-    [...pianoКeys]
+    [...pianoKeys]
       .find((el) => el.dataset.letter === "U")
       .classList.add("piano-key-active");
   }
-  if (event.code === "KeyI") {
+  if (code === "KeyI") {
     const src = `./assets/audio/g♯.mp3`;
     playAudio(src);
-    [...pianoКeys]
+    [...pianoKeys]
       .find((el) => el.dataset.letter === "I")
       .classList.add("piano-key-active");
   }
-  if (event.code === "KeyO") {
+  if (code === "KeyO") {
     const src = `./assets/audio/a♯.mp3`;
     playAudio(src);
-    [...pianoКeys]
+    [...pianoKeys]
       .find((el) => el.dataset.letter === "O")
       .classList.add("piano-key-active");
   }
+});
 
-  window.addEventListener("keyup", () => {
-    [...pianoКeys].map((el) => el.classList.remove("piano-key-active"));
-  });
+window.addEventListener("keyup", () => {
+  [...pianoKeys].map((el) => el.classList.remove("piano-key-active"));
 });
 
 function playAudio(src) {
